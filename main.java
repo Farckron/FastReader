d154bcdfacf25360080b868a7c344d1d0e8685c3
mXn <144520827+Farckron@users.noreply.github.com>
2026-01-07 01:53:13 +0200

    reading file first version done

--- main.java
+++ main.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, World!");
+        File file = new File("input.txt");
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        
     }
 }
