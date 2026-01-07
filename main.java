857a6bb3e98a3cf8a333363ed0f456d3b7b52fe1
mXn <144520827+Farckron@users.noreply.github.com>
2026-01-07 18:02:15 +0200

    added proper input + main -> Main

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Main is running.");
